487355c71a3965580f8dcc673b171c6ea8616a12
Mark Linn <marklinn@xwcsolutions.com>
2012-04-05 12:38:49 -0700

    removed un-needed callouts of stylsheets, etc... since all.css and all.js now encorporate them.  Also put an if block around the homepage images in an attempt to troubleshoot why it's not working on prod server.

--- app/controllers/spree/home_page_controller.rb
+++ app/controllers/spree/home_page_controller.rb
@@ -1,5 +1,5 @@
 module Spree
-  class HomePageController < Spree::BaseController  
+  class HomePageController < Spree::BaseController
     def index
       @homepage_images = HomepageImage.all(:order => 'position')
     end
